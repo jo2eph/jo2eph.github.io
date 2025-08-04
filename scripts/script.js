c9b878105b107096ee7f8ecd752437e15e47f5e1
jo2eph <josephlee@main.local>
2025-08-03 18:35:13 -0700

    Make home page responsive; modify navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,25 @@
-// Get dimension of body
-const body = document.querySelector("body");
+// Breakpoints
+const minWidth = 480;
+const minHeight = 600;
 
+const jumpToTopButton = document.getElementById("jump-to-top");
+
+// Hide navbar menu; display hamburger menu
+window.addEventListener("resize", (event) => {
+    const width = window.innerWidth;
+
+    if (width <= minWidth) {
+        console.log("The screen is getting too small");
+    }
+});
+
+
+// Show or Hide Jump To Top
+window.addEventListener('scroll', (event) => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 200) {
+        jumpToTopButton.style.display = "block";
+    }
+    else {
+        jumpToTopButton.style.display = "none";
+    }
+});
