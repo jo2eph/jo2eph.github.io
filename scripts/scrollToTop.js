4a3aa9b04c54b8fe85117db62258c663507934aa
jo2eph <junhyuk.x.lee@gmail.com>
2026-03-21 01:36:11 -0700

    Finish Section 2 (Education)

--- scripts/scrollToTop.js
+++ scripts/scrollToTop.js
@@ -3,3 +3,12 @@ const jumpToTopButton = document.getElementById("jump-to-top");
 jumpToTopButton.addEventListener("click", () => {
     window.scrollTo({top: 0, behavior: "smooth"});
 });
+
+/* Show button only when user scrolls down */
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        jumpToTopButton.style.display = "block";
+    } else {
+        jumpToTopButton.style.display = "none";
+    }
+})
